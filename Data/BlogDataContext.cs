5ea10c64b26e104677299b5f228d3bd906cd5623
luaan2012 <oempreg.yes@outlook.com.br>
2024-02-11 15:42:07 -0300

    as

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -14,12 +14,23 @@ namespace Blog.Data
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Post> Posts { get; set; } = null!;
         public DbSet<User> Users { get; set; } = null!;
+        public DbSet<PostWithTag> PostWithTags {get; set;} = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryMap());
             modelBuilder.ApplyConfiguration(new UserMap());
             modelBuilder.ApplyConfiguration(new PostMap());
+
+            modelBuilder.Entity<PostWithTag>(x => 
+            {
+                x.ToSqlQuery(@"
+                SELECT 
+                    [Title], 
+                    SELECT COUNT([Id]) FROM [Tags] WHERE [PostId] = [Id] as [Count]
+                FROM 
+                    [Posts]"); 
+            });
         }
     }
 }
---
+++ Models/PostWithTag.cs
@@ -0,0 +1,5 @@
+public class PostWithTag 
+{
+    public int Count { get; set; } = 0;
+    public string Name {get; set;} = string.Empty; 
+}
